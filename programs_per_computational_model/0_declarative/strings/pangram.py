af6d8b367785b174e0b12641062d922586ab19cb
Praise Tompane <praisetompane@icloud.com>
2024-04-06 06:01:43 +0200

    fix(pangrams): declarative and stateful implementations
    --date=0.day.ago

--- programs_per_computational_model/0_declarative/strings/pangram.py
+++ programs_per_computational_model/0_declarative/strings/pangram.py
@@ -3,12 +3,6 @@ import string
 """
     def pangram: a string that contains every letter of the alphabet
 """
-def pangrams(s):
-    unique_characters = set(s.lower())
-    for c in string.ascii_lowercase:
-        if c not in unique_characters:
-            return "not pangram"
-    return "pangram"
 
 def is_subset(s):
     universe = set(s.lower()) # not necessary, this is for set use explicitness
@@ -16,9 +10,6 @@ def is_subset(s):
     if(fixed_domain.issubset(universe)):
         return "pangram"
     return "not pangram"
-
-assert(pangrams("We promptly judged antique ivory buckles for the next prize") == "pangram")
-assert(pangrams("We promptly judged antique ivory buckles") == "not pangram")
 
 assert(is_subset("We promptly judged antique ivory buckles for the next prize") == "pangram")
 assert(is_subset("We promptly judged antique ivory buckles") == "not pangram")
